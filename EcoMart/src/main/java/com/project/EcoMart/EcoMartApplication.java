3f97e3f09140c0333cdc69121ea7c3a5ea805cd4
hari-vk18 <prasathhari972@gmail.com>
2024-12-02 17:48:42 +0530

    First

---
+++ EcoMart/src/main/java/com/project/EcoMart/EcoMartApplication.java
@@ -0,0 +1,13 @@
+package com.project.EcoMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoMartApplication.class, args);
+	}
+
+}
---
+++ EcoMart/src/test/java/com/project/EcoMart/EcoMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.EcoMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
