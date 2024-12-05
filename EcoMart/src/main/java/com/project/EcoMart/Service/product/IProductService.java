34afb762322129d22752abd4a9f3608cdfe6b7b9
hari-vk18 <prasathhari972@gmail.com>
2024-12-05 18:09:05 +0530

    Interface for product

---
+++ EcoMart/src/main/java/com/project/EcoMart/Service/product/IProductService.java
@@ -0,0 +1,29 @@
+package com.project.EcoMart.Service.product;
+
+import java.util.List;
+
+import com.project.EcoMart.Model.Product;
+
+public interface IProductService {
+
+	Product addProduct(Product product);
+
+	Product getProductById(Long id);
+
+	void deleteProductById(Long id);
+
+	void updateProductById(Product product, Long productId);
+
+	List<Product> getAllProducts();
+
+	List<Product> getProductsByCategory(String category);
+
+	List<Product> getProductsByBrand(String brand);
+
+	List<Product> getProductsByCategoryAndBrand(String category, String brand);
+
+	List<Product> getProductsByName(String name);
+
+	List<Product> getProductsByNameAndBrand(String name, String brand);
+
+}
