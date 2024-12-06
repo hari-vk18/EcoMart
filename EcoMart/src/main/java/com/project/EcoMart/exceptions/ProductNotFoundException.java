26de805ce1cbf0856bb6dc260b6292dbf265dbfc
hari-vk18 <prasathhari972@gmail.com>
2024-12-06 17:17:32 +0530

    ProductNotFoundExcep

---
+++ EcoMart/src/main/java/com/project/EcoMart/exceptions/ProductNotFoundException.java
@@ -0,0 +1,10 @@
+package com.project.EcoMart.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {
+
+	public ProductNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
