8b868efe743c6aab912aba2a0abb6b33696033f4
hari-vk18 <prasathhari972@gmail.com>
2024-12-06 18:00:12 +0530

    Service Layer for Product

--- EcoMart/src/main/java/com/project/EcoMart/Service/product/ProductService.java
+++ EcoMart/src/main/java/com/project/EcoMart/Service/product/ProductService.java
@@ -1,5 +1,88 @@
 package com.project.EcoMart.Service.product;
 
-public class ProductService {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.EcoMart.Model.Product;
+import com.project.EcoMart.exceptions.ProductNotFoundException;
+import com.project.EcoMart.repository.ProductRepository;
+import com.project.EcoMart.request.AddProductRequest;
+
+@Service
+public class ProductService implements IProductService {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Override
+	public Product addProduct(AddProductRequest request) {
+		// TODO Auto-generated method stub
+
+		return null;
+	}
+
+	@Override
+	public Product getProductById(Long id) {
+		// TODO Auto-generated method stub
+		return productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException("Product not found!"));
+	}
+
+	@Override
+	public void deleteProductById(Long id) {
+		// TODO Auto-generated method stub
+		productRepository.findById(id).ifPresentOrElse(productRepository::delete, () -> {
+			throw new ProductNotFoundException("Product not found!");
+		});
+	}
+
+	@Override
+	public void updateProductById(Product product, Long productId) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Product> getAllProducts() {
+		// TODO Auto-generated method stub
+		return productRepository.findAll();
+	}
+
+	@Override
+	public List<Product> getProductsByCategory(String category) {
+		// TODO Auto-generated method stub
+		return productRepository.findByCategoryName(category);
+	}
+
+	@Override
+	public List<Product> getProductsByBrand(String brand) {
+		// TODO Auto-generated method stub
+		return productRepository.findByBrand(brand);
+	}
+
+	@Override
+	public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+		// TODO Auto-generated method stub
+		return productRepository.findByCategoryNameAndBrand(category, brand);
+	}
+
+	@Override
+	public List<Product> getProductsByName(String name) {
+		// TODO Auto-generated method stub
+		return productRepository.findByName(name);
+	}
+
+	@Override
+	public List<Product> getProductsByNameAndBrand(String name, String brand) {
+		// TODO Auto-generated method stub
+		return productRepository.findByNameAndBrand(name, brand);
+	}
+
+	@Override
+	public Long countProductsByBrandAndName(String brand, String name) {
+		// TODO Auto-generated method stub
+		return productRepository.countByBrandAndName(brand, name);
+	}
 
 }
