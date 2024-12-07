478d165b656a4a9b78543c8ffe7a6e775627c45d
hari-vk18 <prasathhari972@gmail.com>
2024-12-07 16:02:58 +0530

    Category Repo

---
+++ EcoMart/src/main/java/com/project/EcoMart/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.project.EcoMart.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.EcoMart.Model.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+	Category findByName(String name);
+
+}
