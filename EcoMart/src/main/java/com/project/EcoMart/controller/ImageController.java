716290f11f0d71270bd92edc0107536136ecf716
hari-vk18 <prasathhari972@gmail.com>
2024-12-09 16:54:58 +0530

    Upload end point

--- EcoMart/src/main/java/com/project/EcoMart/controller/ImageController.java
+++ EcoMart/src/main/java/com/project/EcoMart/controller/ImageController.java
@@ -1,8 +1,40 @@
 package com.project.EcoMart.controller;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.project.EcoMart.Service.image.IImageService;
+import com.project.EcoMart.response.ApiResponse;
+
+import dto.ImageDto;
 
 @RestController
+@RequestMapping("${api.prefix}/images")
 public class ImageController {
+
+	@Autowired
+	private IImageService imageService;
+
+	@PostMapping("/upload")
+	public ResponseEntity<ApiResponse> saveImages(@RequestParam List<MultipartFile> files, @RequestParam Long prodid) {
+		try {
+			List<ImageDto> imageDtos = imageService.saveImage(files, prodid);
+			return ResponseEntity.ok(new ApiResponse("Upload success!", imageDtos));
+		} catch (Exception e) {
+			// TODO: handle exception
+			return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Upload Failed!", e.getMessage()));
+		}
+	}
+
+//	public ResponseEntity<ApiResponse> 
 
 }
