f96576fa073db2915106eabc78926bcf6579cc8c
hari-vk18 <prasathhari972@gmail.com>
2024-12-10 17:15:30 +0530

    Upload and Delete end points

--- EcoMart/src/main/java/com/project/EcoMart/controller/ImageController.java
+++ EcoMart/src/main/java/com/project/EcoMart/controller/ImageController.java
@@ -1,7 +1,10 @@
 package com.project.EcoMart.controller;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -11,9 +14,12 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +27,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.project.EcoMart.Model.Image;
 import com.project.EcoMart.Service.image.IImageService;
+import com.project.EcoMart.exceptions.ResourceNotFoundException;
 import com.project.EcoMart.response.ApiResponse;
 
 import dto.ImageDto;
@@ -48,9 +55,44 @@ public class ImageController {
 		Image image = imageService.getImageById(imageId);
 		ByteArrayResource resource = new ByteArrayResource(
 				image.getImage().getBytes(1, (int) image.getImage().length()));
-		return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
-				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + image.getFileName() + "\"")
+		return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType())).header(
+				HttpHeaders.CONTENT_DISPOSITION,
+				"attachment; filename=\"" + URLEncoder.encode(image.getFileName(), StandardCharsets.UTF_8) + "\"")
 				.body(resource);
+	}
+
+	@PutMapping("image/{imageId}/upload")
+	public ResponseEntity<ApiResponse> uploadImage(@PathVariable Long imageId, @RequestBody MultipartFile file) {
+		try {
+			Image image = imageService.getImageById(imageId);
+			if (image != null) {
+				imageService.updateImage(file, imageId);
+				return ResponseEntity.ok().body(new ApiResponse("Update success1", null));
+			}
+
+		} catch (ResourceNotFoundException e) {
+			return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+		}
+
+		return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+				.body(new ApiResponse("Update failed!", INTERNAL_SERVER_ERROR));
+	}
+
+	@DeleteMapping("image/{imageId}/delete")
+	public ResponseEntity<ApiResponse> deleteImage(@PathVariable Long imageId, @RequestBody MultipartFile file) {
+		try {
+			Image image = imageService.getImageById(imageId);
+			if (image != null) {
+				imageService.deleteImageById(imageId);
+				return ResponseEntity.ok().body(new ApiResponse("Delete success!", null));
+			}
+
+		} catch (ResourceNotFoundException e) {
+			return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+		}
+
+		return ResponseEntity.status(INTERNAL_SERVER_ERROR)
+				.body(new ApiResponse("Delete failed!", INTERNAL_SERVER_ERROR));
 	}
 
 }
