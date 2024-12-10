c9816e1b366b9280c78b7c577718ab18da0da99d
hari-vk18 <prasathhari972@gmail.com>
2024-12-10 16:23:17 +0530

    Download endpoint

--- EcoMart/src/main/java/com/project/EcoMart/controller/ImageController.java
+++ EcoMart/src/main/java/com/project/EcoMart/controller/ImageController.java
@@ -2,16 +2,24 @@ package com.project.EcoMart.controller;
 
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.project.EcoMart.Model.Image;
 import com.project.EcoMart.Service.image.IImageService;
 import com.project.EcoMart.response.ApiResponse;
 
@@ -35,6 +43,14 @@ public class ImageController {
 		}
 	}
 
-//	public ResponseEntity<ApiResponse> 
+	@GetMapping("/image/download/{imageId}")
+	public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException {
+		Image image = imageService.getImageById(imageId);
+		ByteArrayResource resource = new ByteArrayResource(
+				image.getImage().getBytes(1, (int) image.getImage().length()));
+		return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + image.getFileName() + "\"")
+				.body(resource);
+	}
 
 }
