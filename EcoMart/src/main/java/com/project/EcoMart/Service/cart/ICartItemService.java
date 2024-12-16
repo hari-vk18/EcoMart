d825585f102d87a5a663b6cb933dc9e8112c452a
hari-vk18 <prasathhari972@gmail.com>
2024-12-16 18:08:42 +0530

    getCartItem

--- EcoMart/src/main/java/com/project/EcoMart/Service/cart/ICartItemService.java
+++ EcoMart/src/main/java/com/project/EcoMart/Service/cart/ICartItemService.java
@@ -1,5 +1,7 @@
 package com.project.EcoMart.Service.cart;
 
+import com.project.EcoMart.Model.CartItem;
+
 public interface ICartItemService {
 
 	void addItemToCart(Long cartId, Long productId, int quantity);
@@ -7,5 +9,7 @@ public interface ICartItemService {
 	void removeItemFromCart(Long cartId, Long productId);
 
 	void updateItemQuantity(Long cartId, Long productId, int quantity);
+
+	CartItem getCartItem(Long cartId, Long productId);
 
 }
