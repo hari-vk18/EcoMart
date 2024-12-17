ea140dc65756f64515573f4b9dc4fe3011ece3c9
hari-vk18 <prasathhari972@gmail.com>
2024-12-17 18:08:24 +0530

    ___

--- EcoMart/src/main/java/com/project/EcoMart/Service/cart/CartItemService.java
+++ EcoMart/src/main/java/com/project/EcoMart/Service/cart/CartItemService.java
@@ -34,21 +34,18 @@ public class CartItemService implements ICartItemService {
 
 		Cart cart = cartService.getCart(cartId);
 		Product product = productService.getProductById(productId);
-
 		CartItem cartItem = cart.getItems().stream().filter(item -> item.getProduct().getId().equals(productId))
 				.findFirst().orElse(new CartItem());
-
 		if (cartItem.getId() == null) {
 			cartItem.setCart(cart);
 			cartItem.setProduct(product);
-			cartItem.setUnitPrice(product.getPrice());
 			cartItem.setQuantity(quantity);
+			cartItem.setUnitPrice(product.getPrice());
 		} else {
 			cartItem.setQuantity(cartItem.getQuantity() + quantity);
 		}
-
 		cartItem.setTotalPrice();
-
+		cart.addItem(cartItem);
 		cartItemRepository.save(cartItem);
 		cartRepository.save(cart);
 
@@ -56,10 +53,7 @@ public class CartItemService implements ICartItemService {
 
 	@Override
 	public void removeItemFromCart(Long cartId, Long productId) {
-		// TODO Auto-generated method stub
-
 		Cart cart = cartService.getCart(cartId);
-
 		CartItem itemToRemove = getCartItem(cartId, productId);
 		cart.removeItem(itemToRemove);
 		cartRepository.save(cart);
@@ -68,17 +62,16 @@ public class CartItemService implements ICartItemService {
 
 	@Override
 	public void updateItemQuantity(Long cartId, Long productId, int quantity) {
-		// TODO Auto-generated method stub
-		Cart cart = cartRepository.getById(cartId);
-
+		Cart cart = cartService.getCart(cartId);
 		cart.getItems().stream().filter(item -> item.getProduct().getId().equals(productId)).findFirst()
 				.ifPresent(item -> {
 					item.setQuantity(quantity);
 					item.setUnitPrice(item.getProduct().getPrice());
 					item.setTotalPrice();
 				});
+		BigDecimal totalAmount = cart.getItems().stream().map(CartItem::getTotalPrice).reduce(BigDecimal.ZERO,
+				BigDecimal::add);
 
-		BigDecimal totalAmount = cart.getTotalAmount();
 		cart.setTotalAmount(totalAmount);
 		cartRepository.save(cart);
 	}
@@ -87,7 +80,7 @@ public class CartItemService implements ICartItemService {
 	public CartItem getCartItem(Long cartId, Long productId) {
 		Cart cart = cartService.getCart(cartId);
 		return cart.getItems().stream().filter(item -> item.getProduct().getId().equals(productId)).findFirst()
-				.orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+				.orElseThrow(() -> new ResourceNotFoundException("Item not found"));
 
 	}
 
