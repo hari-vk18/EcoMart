c3dcc8527108a730eb45246f0d0830cbbdfd6fce
hari-vk18 <prasathhari972@gmail.com>
2024-12-17 18:08:57 +0530

    initializeNewCart

--- EcoMart/src/main/java/com/project/EcoMart/Service/cart/CartService.java
+++ EcoMart/src/main/java/com/project/EcoMart/Service/cart/CartService.java
@@ -1,7 +1,10 @@
 package com.project.EcoMart.Service.cart;
 
 import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,22 +13,28 @@ import com.project.EcoMart.exceptions.ResourceNotFoundException;
 import com.project.EcoMart.repository.CartItemRepository;
 import com.project.EcoMart.repository.CartRepository;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class CartService implements ICartService {
 
 	@Autowired
-	private CartRepository cartRepo;
+	private CartRepository cartRepository;
 
 	@Autowired
 	private CartItemRepository cartItemRepository;
 
+	private AtomicLong cartIdGenerator = new AtomicLong(0);
+
+	private static final Logger log = LoggerFactory.getLogger(CartService.class);
+
 	@Override
 	public Cart getCart(Long id) {
 		// TODO Auto-generated method stub
-		Cart cart = cartRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cart Not found!"));
+		Cart cart = cartRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Cart not found"));
 		BigDecimal totalAmount = cart.getTotalAmount();
 		cart.setTotalAmount(totalAmount);
-		return cartRepo.save(cart);
+		return cartRepository.save(cart);
 	}
 
 	@Override
@@ -34,15 +43,24 @@ public class CartService implements ICartService {
 		Cart cart = getCart(id);
 		cartItemRepository.deleteAllByCartId(id);
 		cart.getItems().clear();
-		cartRepo.deleteById(id);
+		cartRepository.deleteById(id);
 
 	}
 
 	@Override
 	public BigDecimal getTotalPrice(Long id) {
-		// TODO Auto-generated method stub
 		Cart cart = getCart(id);
 		return cart.getTotalAmount();
+	}
+
+	@Transactional
+	@Override
+	public Long initializeNewCart() {
+		Cart newCart = new Cart();
+		Long newCartId = cartIdGenerator.incrementAndGet();
+		newCart.setId(newCartId);
+		cartRepository.save(newCart);
+		return newCartId;
 	}
 
 }
