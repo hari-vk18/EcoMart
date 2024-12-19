fee7c1a8f57e0d64ea03db29cae5538b387a27ce
hari-vk18 <prasathhari972@gmail.com>
2024-12-19 17:40:32 +0530

    order service

---
+++ EcoMart/src/main/java/com/project/EcoMart/Service/order/OrderService.java
@@ -0,0 +1,40 @@
+package com.project.EcoMart.Service.order;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.project.EcoMart.Model.Cart;
+import com.project.EcoMart.Model.Order;
+import com.project.EcoMart.Model.OrderItem;
+import com.project.EcoMart.exceptions.ResourceNotFoundException;
+import com.project.EcoMart.repository.OrderRepository;
+
+@Service
+public class OrderService implements IOrderService {
+
+	private OrderRepository orderRepo;
+
+	@Override
+	public Order placeOrder(Long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private List<OrderItem> createOrderItems(Order order, Cart cart) {
+
+	}
+
+	private BigDecimal calculateTotalAmount(List<OrderItem> orderItemList) {
+		return orderItemList.stream().map(item -> item.getPrice().multiply(new BigDecimal(item.getQuantity())))
+				.reduce(BigDecimal.ZERO, BigDecimal::add);
+	}
+
+	@Override
+	public Order getOrder(Long orderId) {
+		// TODO Auto-generated method stub
+		return orderRepo.findById(orderId).orElseThrow(() -> new ResourceNotFoundException("Order not exist"));
+	}
+
+}
